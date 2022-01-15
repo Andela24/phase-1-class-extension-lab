161a4376d83a811e4287fc513de102dea720e7f0
Andela Stevanovic <andjela.s166@gmail.com>
2022-01-14 19:08:47 -0500

    commit tree 0631977250ce094c399bf760e671d6ebbaf7a1f7

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+class Polygon {
+    constructor(arr) {
+        this.sides = arr
+    }
+get countSides(){
+    return this.sides.length
+}
+get perimeter(){
+return this.sides.reduce((side, total) => total += side ) //adding a each side
+}
+}
+class Triangle extends Polygon{
+get isValid() {
+    if(this.countSides === 3) {
+    
+        this.sides[0] + this.sides[1] > this.sides[2] && this.sides[1] + this.sides[2] > this.sides[3] && this.sides[3] + this.sides[2] > this.sides[1]
+    }
+}
+}
+class Square extends Polygon{
+    get area() {
+
+return this.sides[0] * this.sides[1]
+    }
+
+    get isValid() {
+        let test = this.sides[0]
+        let count = 1
+for(let i = 1; i < this.sides.length; i++) {
+ test === this.sides[i] ? count += 1 : null
+    }
+    return this.sides.length === count
+}
+}
